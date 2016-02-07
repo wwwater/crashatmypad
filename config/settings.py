d1ff6a0aa4851ed44711facf45f4eab6e43025c4
wwwater <daria.shubina@gmail.com>
2016-02-07 11:59:46 +0100

    Add config file back

---
+++ config/settings.py
@@ -0,0 +1,9 @@
+SECRET_KEY = 'pickabettersecret'
+
+APP_NAME = 'Crash at my Pad'
+
+DEBUG = True
+TESTING = False
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://crashatmypad:padmypad@postgres:5432/crashpads'
+REDIS_URL = 'redis://redis:6379/0'
