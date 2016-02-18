647111b1ea0a2e8d98e54e9594815f8b065acbcd
cyclinginthewind <daria.shubina@gmail.com>
2016-02-18 20:32:50 +0100

    Change relative config flag

--- crashatmypad/app.py
+++ crashatmypad/app.py
@@ -22,10 +22,10 @@ def create_app():
 
     :return: Flask app
     """
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__, instance_relative_config=False)
 
     app.config.from_object('config.settings')
-    app.config.from_pyfile('settings.py', silent=True)
+    app.config.from_pyfile('settings.py', silent=False)
 
     db.init_app(app)
     redis_store.init_app(app)
