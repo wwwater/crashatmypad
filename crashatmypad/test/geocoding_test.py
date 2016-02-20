ae30dda2f825c59bc39e3a50961b6b20e54572c1
cyclinginthewind <daria.shubina@gmail.com>
2016-02-20 16:45:01 +0100

    Tests for geo coding

--- crashatmypad/test/geocoding_test.py
+++ crashatmypad/test/geocoding_test.py
@@ -3,26 +3,26 @@ from crashatmypad.util.geocode import geocode
 
 def test_geocode_full_address():
     data = \
-        geocode(country='Germany', postal_code='20359', city='Hamburg',
-                street='Budapesterstr.', house='45')
+        geocode(country='Germany', postal_code='22299', city='Hamburg',
+                street='Barmbekerstr.', house='169')
     assert len(data) == 1
-    assert float('{0:.2f}'.format(float(data[0]['lon']))) == 9.96
-    assert float('{0:.2f}'.format(float(data[0]['lat']))) == 53.56
+    assert float('{0:.2f}'.format(float(data[0]['lon']))) == 10.00
+    assert float('{0:.2f}'.format(float(data[0]['lat']))) == 53.59
 
 
 def test_geocode_wrong_postal_code():
     data = \
-        geocode(country='Germany', postal_code='30359', city='Hamburg',
-                street='Budapesterstr.', house='45')
+        geocode(country='Germany', postal_code='22290', city='Hamburg',
+                street='Barmbekerstr.', house='169')
     assert len(data) == 0
 
 
 def test_geocode_street_only():
     data = \
-        geocode(country='Germany', city='Hamburg', street='Budapesterstr.')
+        geocode(country='Germany', city='Hamburg', street='Barmbekerstr.')
     assert len(data) == 1
-    assert float('{0:.2f}'.format(float(data[0]['lon']))) == 9.97
-    assert float('{0:.2f}'.format(float(data[0]['lat']))) == 53.55
+    assert float('{0:.2f}'.format(float(data[0]['lon']))) == 10.01
+    assert float('{0:.2f}'.format(float(data[0]['lat']))) == 53.59
 
 
 test_geocode_full_address()
